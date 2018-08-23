adcc5b159033bd4f5335388417c89639603584d0
github username <github email address>
2018-08-23 09:45:37 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,14 @@ Card.create(category: "self", name: "The Emperor", saying:
 "Control and stability. Spread your influence.")
 Card.create(category: "self", name: "The Hanged Man", saying:
 "Break old patterns for new opportunity.")
+Card.create(category: "self", name: "The Ace Of Swords", saying:
+"Success in your life is bound up by generosity.")
+Card.create(category: "self", name: "The King Of Wands", saying:
+"Be open to the advice of those with more experience than yourself.")
+Card.create(category: "self", name: "The Ace Of Swords", saying:
+"Change through conquering internal forces leads to new beginnings.")
+Card.create(category: "self", name: "The Five of Pentacles", saying:
+"This is time for you to carefully evaluate and work out your priorities.")
 
 ##### LOVE CARDS #####
 Card.create(category: "love", name: "The High Priestess", saying:
@@ -23,6 +31,11 @@ Card.create(category: "love", name: "The Fool", saying:
 "You’re learning, but you’re just not quite there yet.")
 Card.create(category: "love", name: "Strength", saying:
 "You a strong independent woman that don't need no man.")
+Card.create(category: "love", name: "The Star", saying:
+"Sometimes letting go is the best option. It will open up new possibilites.")
+Card.create(category: "love", name: "The Lovers", saying:
+"Make the best out of the relationship after the initial bliss is gone.")
+
 
 ##### CAREER CARDS #####
 Card.create(category: "career", name: "The Devil", saying:
@@ -35,6 +48,12 @@ Card.create(category: "career", name: "The Hierophant", saying:
 "Your job is boring now, try a different field.")
 Card.create(category: "career", name: "The Tower", saying:
 "You can get further from your position, all you have to do is climb up.")
+Card.create(category: "career", name: "Judgement", saying:
+"You are overwhelmed by a decision. Your decision shouldn't be made lightly.")
+Card.create(category: "career", name: "The Ten of Pentacles", saying:
+"Inheritance or retirement is foreseen.")
+Card.create(category: "career", name: "The Nine of Pentacles", saying:
+"Your hard work and careful planning will be rewarded.")
 
 ##### FUTURE CARDS #####
 Card.create(category: "future", name: "Eight Of Swords", saying:
@@ -47,3 +66,19 @@ Card.create(category: "future", name: "The Chariot", saying:
 "Freedom will come to you soon.")
 Card.create(category: "future", name: "Two Of Wands", saying:
 "In the future, you will meet someone who will help you win the lottery.")
+Card.create(category: "future", name: "Death", saying:
+"Something is coming to an end. Raise your awareness.")
+Card.create(category: "future", name: "The Ace Of Pentacles", saying:
+"Emotional stability, financial gain, luck, recognition, and success.")
+Card.create(category: "future", name: "The Six Of Swords", saying:
+"It may be helpful to seek guidance, but the worst is over.")
+Card.create(category: "future", name: "The Nine Of Swords", saying:
+"Despair or a powerful mental anguish foreseen. Be alert.")
+Card.create(category: "future", name: "The Four Of Swords", saying:
+"Offer your withdrawal from certain battles in your life.")
+
+##### PRESENT CARDS #####
+Card.create(category: "present", name: "The Ten of Cups", saying:
+"You can expect achievement and to attain some long goal.")
+Card.create(category: "present", name: "The Four of Wands", saying:
+"Something in your life is not what it seems.")
